7c3208c840e169e0327c6b2996fd3f46923a2b65
Valentina Zavodova <106472523+ValentinaBelle@users.noreply.github.com>
2026-04-16 20:58:13 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 import moviepy
 from moviepy import VideoFileClip, TextClip
+from pathlib import Path
 
-video = moviepy.VideoFileClip("name of you file.mp4")
+# audio will inherit the name of the video
+video_file = Path("the_name_of_the_video.mp4")
+video = moviepy.VideoFileClip(f"{video_file}")
 audio = video.audio
-audio.write_audiofile("my_audio.mp3")
+audio.write_audiofile((f{"video_file.stem}).mp3")
 
 # Cleanup
 video.close()
